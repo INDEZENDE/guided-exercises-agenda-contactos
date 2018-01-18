8f7b18e27f4e51b3152cbdfc6e8ae6fd00ad6358
biokah <maria.anaisr@gmail.com>
2018-01-18 15:31:36 -0600

    function add and validate

--- js/app.js
+++ js/app.js
@@ -1,61 +1,69 @@
+//Traer elementos del HTML
+var $nameInput = $('#name-input');
+var $phoneInput = $('#phone-input');
 
-  //Traer elementos del HTML
-  
-  
-  var loadPage = function () {
-    
-  };
+function loadPage() {
+    $('.modal').modal();
+    $nameInput.keyup(validateContact)
+    $phoneInput.keyup(validateContact)
+};
 
-  var paintContactsInHTML = function(contact) {
-   
+var paintContactsInHTML = function(contact) {
+
     /* Crear elementos con DOM html al publicar contacto */
-     
-    
+
+
     //Asignando atributos y/o eventos
-    
+
 
     /* Asignando valores a los elementos*/
-    
+
 
     //Agregamos lo que creamos con el DOM a un elemento existente del HTML
-    
-  };
 
-  var addContact = function (e) {
+};
+
+var addContact = function(e) {
     e.preventDefault();
     //Las siguientes lineas toman el valor que el usuario agrega en los inputs y los guardan en variables
-    
+
 
     //Con las variables separadas que obtuvimos creamos una estructura de datos unica, un objeto por cada contacto
-    
+
 
     /*Agregamos el contacto a nuestra data (arreglo declarado) para poderla filtrar y eliminar posteriormente*/
-    
+
     //La siguiente funcion se encarga de pintar los contactos en el html
-    
+
     /* Limpiando valores de formulario*/
-    
-  };
 
-  
-
-
-  var validateContact = function () {
-    //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
-  };
-
-
-  var filterContacts = function() {
-    //Esta funcion debe de filtrar la data segun el valor que el usuario ingrese en el input de busqueda
-  };
-
-
-  var removeContact = function () {
-    //esta funcion como primer alcance debe de poer borrar la tarjeta que se crea desde el DOM
-    //como segundo alcance borrar el elemento de la data
-  };
+};
 
 
 
 
-  $(document).ready(loadPage);
+function validateContact() {
+    if ($(this).val().trim().length > 0) {
+        $('#add-contact').removeAttr('disabled')
+    } else {
+        $('#add)
+        }
+
+    }
+
+
+
+    var filterContacts = function() {
+        //Esta funcion debe de filtrar la data segun el valor que el usuario ingrese en el input de busqueda
+    };
+
+
+    var removeContact = function() {
+        //esta funcion como primer alcance debe de poer borrar la tarjeta que se crea desde el DOM
+        //como segundo alcance borrar el elemento de la data
+    };
+
+
+
+
+    $(document).ready(loadPage);
